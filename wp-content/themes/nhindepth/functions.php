5060f3f09f2a4ffb9d897f482e483e163b41c1cb
aschweigert <aschweigert@gmail.com>
2016-11-30 10:13:41 -0500

    hosting updates

--- wp-content/themes/nhindepth/functions.php
+++ wp-content/themes/nhindepth/functions.php
@@ -1,6 +1,17 @@
 <?php
-
-function my_scripts_method() {
+/**
+ * Constants
+ */
+// This site is an INN Member
+if ( ! defined( 'INN_MEMBER' ) ) {
+    define( 'INN_MEMBER', true );
+}
+// This site is hosted by INN
+if ( ! defined( 'INN_HOSTED' ) ) {
+    define( 'INN_HOSTED', true );
+}
+	
+function nhindepth_enqueue_scripts() {
 	wp_enqueue_script(
 		'custom-script',
 		get_stylesheet_directory_uri() . '/js/indepthnh.js',
@@ -8,4 +19,4 @@ function my_scripts_method() {
 	);
 }
 
-add_action( 'wp_enqueue_scripts', 'my_scripts_method' );
+add_action( 'wp_enqueue_scripts', 'nhindepth_enqueue_scripts' );
